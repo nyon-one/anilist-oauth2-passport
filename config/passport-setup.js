938e7e9e32c6d511650752888177bc6128409a78
nyon-one <one.nyon@gmail.com>
2019-06-14 15:48:43 +0545

    Clean:code

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 
 const passport = require('passport')
 const OAuth2Strategy = require('passport-oauth2')
+const AnilistStrategy = require('./anilist-strategy')
+
+console.log(AnilistStrategy)
 
 passport.serializeUser((user, done) => {
     done(null, user)
@@ -13,35 +16,30 @@ passport.deserializeUser((id, done) => {
 
 const graphApi = require('./api')
 
-passport.use(new OAuth2Strategy({
-        authorizationURL: 'https://anilist.co/api/v2/oauth/authorize',
-        tokenURL: 'https://anilist.co/api/v2/oauth/token',
-        clientID: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: 'http://localhost:5000/auth/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
+passport.use(AnilistStrategy((accessToken, refreshToken, profile, done) => {
         console.log('============')
         console.log(accessToken)
         console.log('============')
-        graphApi(accessToken).post('', {
-                query: 'query{Viewer{id, name, avatar{large}}}'
-            })
-            .then(r => {
-                const user = r.data.data.Viewer;
-                user.accessToken = accessToken
+}))
 
-                done(null, user)
-            })
-        // .catch(r => console.log(r.data))
+//         graphApi(accessToken).post('', {
+//                 query: 'query{Viewer{id, name, avatar{large}}}'
+//             })
+//             .then(r => {
+//                 const user = r.data.data.Viewer;
+//                 user.accessToken = accessToken
 
-        // return cb(null, {
-        //     user: 1
-        // });
-        // User.findOrCreate({
-        //     exampleId: profile.id
-        // }, function (err, user) {
-        //     return cb(err, user);
-        // });
-    }
-));
+//                 done(null, user)
+//             })
+//         // .catch(r => console.log(r.data))
+
+//         // return cb(null, {
+//         //     user: 1
+//         // });
+//         // User.findOrCreate({
+//         //     exampleId: profile.id
+//         // }, function (err, user) {
+//         //     return cb(err, user);
+//         // });
+//     }
+// ));
